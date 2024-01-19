fbd14f18b85adef0f8cc1ead6840a2f77b78607f
Aditya Kumar Sharma <sharmaadityakumar207@gmail.com>
2024-01-19 20:50:27 +0530

    SOME CHANGE

--- JS/index.js
+++ JS/index.js
@@ -1,18 +1,77 @@
-var swiper = new Swiper(".slide-content",{
-    slidesPerView: 3,
-    spaceBetween: 8,
-    slidesPerGroup: 3,
-    loop: true,
-    centerSlide:'true',
-    fade: 'true',
-    gragCursor: 'true',
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        dynamicBullets: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-});
+// var swiper = new Swiper(".slide-content",{
+//     slidesPerView: 3,
+//     spaceBetween: 8,
+//     slidesPerGroup: 3,
+//     loop: true,
+//     centerSlide:'true',
+//     fade: 'true',
+//     gragCursor: 'true',
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//         dynamicBullets: true,
+//     },
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     },
+// });
+
+const gallleryContainer = document.querySelector('.gallery-container');
+const gallleryControlsContainer = document.querySelector('.gallery-controls');
+const gallleryControls = ['previous','next'];
+const gallleryItems = document.querySelector('.gallery-item');
+
+class Carousel {
+
+    constructor(container, items, controls){
+        this.carouselContainer = container;
+        this.carouselControls = controls;
+        this.carouselArray = [...items];
+    }
+
+    updateGallery(){
+        this.carouselArray.forEach(el => {
+            el.classList.remove('gallery-item-1');
+            el.classList.remove('gallery-item-2');
+            el.classList.remove('gallery-item-3');
+            el.classList.remove('gallery-item-4');
+            el.classList.remove('gallery-item-5');
+        });
+
+        this.carouselArray.slice(0,5).forEach((el , i) => {
+            el.classList.add(`gallery-item-${i+1}`);
+        });
+    }
+
+    setCurrentState(direction){
+        if(direction.className == 'gallery-controls-previous'){
+            this.carouselArray.unshift(this.carouselArray.pop());
+        }else{
+            this.carouselArray.push(this.carouselArray.shift());
+        }
+        this.updateGallery();
+    }
+
+    setControls(){
+        this.carouselControls.forEach(control => {
+            gallleryControlsContainer.appendChild(document.createElement('button')).className = `gallery-controls-${control}`;
+            document.querySelector(`.gallery-controls-${control}`).innerText = control;
+        })
+    }
+
+    useControls(){
+        const triggers = [...gallleryControlsContainer.childNodes];
+        triggers.forEach(control => {
+            control.addEventListener('click', e => {
+                e.preventDefault();
+                this.setCurrentState(control);
+            });
+        });
+    }
+}
+
+const exampleCarousel = new Carousel(gallleryContainer, gallleryItems, gallleryControls);
+
+exampleCarousel.setControls();
+exampleCarousel.useControls();
